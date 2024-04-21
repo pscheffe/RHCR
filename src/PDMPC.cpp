d347a1bf0d1cf001bd100e690ba7932c8a3c559f
fbelderink <fynnmaarten.business@gmail.com>
2024-04-21 22:06:10 +0200

    added documentation

--- src/PDMPC.cpp
+++ src/PDMPC.cpp
@@ -2,9 +2,21 @@
 
 #include <random>
 
+/**
+ * @brief Constructs a new PDMPC object.
+ * @param G A BasicGraph object.
+ */
 PDMPC::PDMPC(const BasicGraph &G)
     : G {G} { }
 
+/**
+* @brief Gets the sequentially planning agents.
+* @param agent_states A vector of agent states.
+* @param window An integer representing the window.
+* @param max_num_CLs An integer representing the maximum number of computation levels.
+* @return A tuple containing the directed coupling graph and the directed sequential
+ * coupling graph and a vector of reachable set objects.
+*/
 tuple<Graph, Graph, vector<bitset_t>> PDMPC::get_sequentially_planning_agents(const vector<State> &agent_states, const int &window, const int &max_num_CLs) const {
     auto[coupling, reachable_sets] = this->get_coupling_graph(agent_states, window);
 
@@ -17,6 +29,15 @@ tuple<Graph, Graph, vector<bitset_t>> PDMPC::get_sequentially_planning_agents(co
     return make_tuple(directed_coupling, directed_sequential_coupling, reachable_sets);
 }
 
+/**
+ *
+ * @brief Gets the computation levels for a given directed coupling graph.
+ * @description Kahn's algorithm is used to find the computation levels of the agents.
+ * In each step we assign an increasing computation level to the to-be cut vertices
+ * starting from level 1 until we have assigned a level to all vertices.
+ * @param directed_coupling_graph A Graph object.
+ * @return An optional vector of integers with the agents computation level at the agents index.
+ */
 boost::optional<vector<int>> PDMPC::get_computation_levels(const Graph &directed_coupling_graph) {
     auto num_agents = directed_coupling_graph.size();
     Graph A(directed_coupling_graph);
@@ -63,6 +84,15 @@ boost::optional<vector<int>> PDMPC::get_computation_levels(const Graph &directed
     return computation_levels;
 }
 
+/**
+ * @brief Gets the reachable set for a given initial state and window.
+ * @description The reachable set is calculated by performing a breadth-first search.
+ * The reachable set is constructed by flattening the map (encoded in a BasicGraph)
+ * into the bitset and setting the bits at reachable tiles (represent by the flattened indices) to 1
+ * @param initial_state A State object representing the initial state.
+ * @param window An integer representing the window.
+ * @return A bitset_t object which represents the reachable set.
+ */
 bitset_t PDMPC::get_reachable_set(const State &initial_state, const int &window) const
 {
     bitset_t reachable_set(G.size());
@@ -92,6 +122,14 @@ bitset_t PDMPC::get_reachable_set(const State &initial_state, const int &window)
     return reachable_set;
 }
 
+/**
+ * @brief Gets the coupling graph for a given vector of agent states and a window.
+ * @description The coupling graph is constructed by checking if the reachable sets of the agents intersect.
+ * If the reachable sets intersect, the agents are coupled, which means there is an edge between the agents.
+ * @param agent_states A vector of State objects representing the agent states.
+ * @param window An integer representing the window.
+ * @return A pair containing a the coupling and a vector of reachable set objects.
+ */
 pair<Graph, vector<bitset_t>> PDMPC::get_coupling_graph(const vector<State> &agent_states, const int &window) const {
 
     auto num_agents = agent_states.size();
@@ -116,6 +154,11 @@ pair<Graph, vector<bitset_t>> PDMPC::get_coupling_graph(const vector<State> &age
     return make_pair(coupling, reachable_sets);
 }
 
+/**
+ * @brief Prioritizes a given coupling graph by ordering the agents randomly.
+ * @param coupling_graph a coupling graph.
+ * @return a directed graph with edges from higher to lower priority agents.
+ */
 Graph PDMPC::prioritize(const Graph &coupling_graph) const {
     auto num_agents = coupling_graph.size();
 
@@ -140,6 +183,15 @@ Graph PDMPC::prioritize(const Graph &coupling_graph) const {
     return directed_coupling_graph;
 }
 
+/**
+ * @brief Weighs a given directed coupling graph by distance.
+ * @description The weights are calculated by taking the inverse of the distance between the agents.
+ * Therefore the distances are divided by a maximum distance of two agents, which is determined by 2*window
+ * @param directed_coupling_graph A directed coupling graph.
+ * @param agent_states A vector of State objects representing the agent states.
+ * @param window An integer representing the window.
+ * @return A WeightedGraph object with close agents having higher weights.
+ */
 WeightedGraph<double> PDMPC::weigh(const Graph &directed_coupling_graph, const vector<State> &agent_states, const int &window) const {
     WeightedGraph<double> directed_weighted_coupling_graph(directed_coupling_graph.size(), vector<double>(directed_coupling_graph.size()));
 
@@ -159,6 +211,12 @@ WeightedGraph<double> PDMPC::weigh(const Graph &directed_coupling_graph, const v
     return directed_weighted_coupling_graph;
 }
 
+/**
+ * @brief Group agents greedily (in decreasing order) by trying to sequentialize edges of closer agents first.
+ * @param directed_weighted_coupling_graph A WeightedGraph object.
+ * @param max_num_CLs An integer representing the maximum number of computation levels.
+ * @return A Graph object with edges between sequentially computing agents.
+ */
 template<typename T>
 Graph PDMPC::group(const WeightedGraph<T> &directed_weighted_coupling_graph, const int &max_num_CLs) const {
     auto num_agents = directed_weighted_coupling_graph.size();
@@ -231,6 +289,12 @@ template Graph PDMPC::group(const vector<vector<double>> &directed_weighted_coup
 
 //debugging
 
+/**
+ * @brief Prints the map with '1' in place of the reachable set.
+ * @param reachable_set A bitset_t object representing the reachable set.
+ * @param start_location An integer representing the start location. Default is -1.
+ * @param headline A string representing the headline. Default is "reachable set".
+ */
 void PDMPC::print_reachable_set(const bitset_t &reachable_set, const int &start_location, const string &headline) const {
     print_headline(headline);
     for(int i = 0; i < G.get_rows(); i++) {
@@ -247,6 +311,11 @@ void PDMPC::print_reachable_set(const bitset_t &reachable_set, const int &start_
     std::cout << std::endl;
 }
 
+/**
+ * @brief Prints the matrix representing a coupling graph.
+ * @param graph A WeightedGraph object.
+ * @param headline A string representing the headline. Default is "weighted coupling graph".
+ */
 template<typename T>
 void PDMPC::print_coupling_graph(const WeightedGraph<T> &graph, const string &headline) const {
     print_headline(headline);
@@ -261,6 +330,11 @@ void PDMPC::print_coupling_graph(const WeightedGraph<T> &graph, const string &he
 template void PDMPC::print_coupling_graph(const WeightedGraph<int> &graph, const string &headline) const;
 template void PDMPC::print_coupling_graph(const WeightedGraph<double> &graph, const string &headline) const;
 
+/**
+ * @brief Prints the matrix representing a coupling graph.
+ * @param graph A Graph object.
+ * @param headline A string representing the headline. Default is "coupling graph".
+ */
 void PDMPC::print_coupling_graph(const Graph &graph, const string &headline) const {
     vector<vector<int>> g_v = vector<vector<int>>(graph.size(), vector<int>(graph.size()));
     for(int i = 0; i < g_v.size(); i++) {
@@ -271,6 +345,10 @@ void PDMPC::print_coupling_graph(const Graph &graph, const string &headline) con
     print_coupling_graph(g_v, headline);
 }
 
+/**
+ * @brief Prints a headline.
+ * @param headline A string representing the headline.
+ */
 void PDMPC::print_headline(const std::string &headline) const {
     std::cout << "--------------------------------------------------" << std::endl;
     std::cout << headline << std::endl;
